11593d9e1b4a0b9a357f827821720b9f718d088e
reecemill <99686103+reecemill@users.noreply.github.com>
2024-10-09 12:31:13 -0500

    Update Fighter.java

--- Fighter.java
+++ Fighter.java
@@ -1,12 +1,15 @@
-/*
+e/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany._prog1;
 
 /**
- *
- * @author scorn
+ * Battle
+ * Attacks: Creates the attacks and defense choices
+ * Attributes: This gives each fighter their values for damages and defensive stats 
+ * Choices: This can be different attack options or defending options based on the decision 
+ *that the player/AI chooses.
  */
 public class Fighter {
     
