a02e58f7881b40b302adc5280480f5b9a400dc45
reecemill <99686103+reecemill@users.noreply.github.com>
2024-10-09 12:27:01 -0500

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -5,8 +5,11 @@
 package com.mycompany._prog1;
 
 /**
- *
- * @author scorn
+ * Battle
+ * Attacks: Creates the attacks and defense choices
+ * Attributes: This gives each fighter their values for damages and defensive stats 
+ * Choices: This can be different attack options or defending options based on the decision 
+ *that the player/AI chooses.
  */
 public class Battle {
     public Battle(){}
