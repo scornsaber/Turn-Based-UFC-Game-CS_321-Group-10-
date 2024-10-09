4b4ff111e3158b5972f683aa45fdd303d4493f6b
robotic toad <scornsaber@users.noreply.github.com>
2024-10-09 12:55:48 -0500

    Update Fighter_CPU.java

--- Fighter_CPU.java
+++ Fighter_CPU.java
@@ -4,10 +4,12 @@
  */
 package com.mycompany._prog1;
 
-/**
+/** Fighter CPU Management
  *
  * @author scorn
- */
+    This is depending on the PVP part working and we have the time
+    Method that will choose a decision based off the the players choice.
+*/
 public class Fighter_CPU {
     public Fighter_CPU(){}
 }
