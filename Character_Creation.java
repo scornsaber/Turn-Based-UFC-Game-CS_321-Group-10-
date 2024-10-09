ed54421b389259589d89cc90e9f6e8f666a1ca44
robotic toad <scornsaber@users.noreply.github.com>
2024-10-09 11:51:26 -0500

    Add files via upload

---
+++ Character_Creation.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._prog1;
+
+/**
+ *
+ * @author scorn
+ */
+public class Character_Creation {
+    public Character_Creation(){}
+}
+    
