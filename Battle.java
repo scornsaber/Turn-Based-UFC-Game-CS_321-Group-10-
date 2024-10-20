2c552eae1155bdf868b56dd88e4bebadadcdc292
robotic toad <scornsaber@users.noreply.github.com>
2024-10-20 14:12:31 -0500

    Update Battle.java with main loop

--- Battle.java
+++ Battle.java
@@ -10,6 +10,40 @@ package com.mycompany._prog1;
  *attack/defense
  */
 public class Battle {
-    public Battle(){}
-    
+    private Fighter fighter1;
+    private Fighter fightet2;
+
+    // Constructer
+    public Battle(Fighter fighter1, Fighter fighter2) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        
+    }
+    public void BeginBattle(){
+        System.out.println("Battle begin message");
+        boolean battleOngoing = true;
+
+        //Loop body is just a for instance/place holder
+        while (battleOngoing) {
+            double damageDealt = fighter1.attack(fighter2);
+            System.out.println(fighter1.getName() + " attacks " + fighter2.getName() + " dealing " + damageDealt + " damage!");
+        }
+
+            if (fighter2.getHealth() <= 0) {
+                System.out.println(fighter1.getName() + " wins the battle!");
+                battleOngoing = false;
+                break;
+            }
+
+            // Fighter 2 attacks Fighter 1
+            damageDealt = fighter2.attack(fighter1);
+            System.out.println(fighter2.getName() + " attacks " + fighter1.getName() + " dealing " + damageDealt + " damage!");
+            System.out.println(fighter1.getName() + "'s health: " + fighter1.getHealth());
+
+            if (fighter1.getHealth() <= 0) {
+                System.out.println(fighter2.getName() + " wins the battle!");
+                battleOngoing = false;
+            }
+            
+    }
 }
