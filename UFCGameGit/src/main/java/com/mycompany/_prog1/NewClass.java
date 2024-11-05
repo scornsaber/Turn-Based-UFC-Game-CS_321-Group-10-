d5eb108adcdf60dd0b2fbf4903d5926db083789a
superwatts <ethan@gorbus>
2024-11-05 14:56:54 -0600

    Test 2:56PM

---
+++ UFCGameGit/src/main/java/com/mycompany/_prog1/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany._prog1;
+
+/**
+ * Place Holder
+ * 
+ */
+public class newclass extends Fighter {
+    
+}
